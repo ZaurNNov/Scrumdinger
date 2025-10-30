e5c986be38928815993335c0e0ad27589679ba21
Zaur Giyasov <zaurusnnov@icloud.com>
2025-10-30 15:59:15 +0300

    3.1. - Creating a navigation hierarchy NavigationLink, NavigationStack, .navigationTitle

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,52 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Zaur on 30.10.2025.
+//
+
+import SwiftUI
+import ThemeKit
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Lenght", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendes")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
