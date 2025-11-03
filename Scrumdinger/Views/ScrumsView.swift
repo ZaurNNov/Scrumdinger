6199952f95f386a029177531a585be9a32016db4
Zaur Giyasov <zaurusnnov@icloud.com>
2025-11-03 13:04:14 +0300

    5.4 - Updating app data

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -10,6 +10,7 @@ import ThemeKit
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @State private var isPresentingNewScrumView = false
     
     var body: some View {
         NavigationStack {
@@ -21,11 +22,16 @@ struct ScrumsView: View {
             }
             .navigationTitle("Daily Scrums")
             .toolbar() {
-                Button(action: {}) {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }) {
                     Image(systemName: "plus")
                 }
                 .accessibilityLabel("New scrum")
             }
+        }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NewScrumSheet(scrums: $scrums)
         }
     }
 }
